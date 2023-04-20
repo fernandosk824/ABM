77115e1ead20481966ccce3c6bcabf6353752540
fernando bonilla <fernandosk824@hotmail.com>
2023-04-20 13:59:28 -0300

    inicial

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+//seteamos el motor de plantillas
+app.set('view engine', 'ejs')
+
+//seteamos la carpeta public para archivos estaticos
+app.use(express.static('public'))
+
+//para procesar datos enviados desde formas
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+//seteamos las variables de entorno
+dotenv.config({path: './env/.env'})
+
+//para poder trabajar con las cookies
+//app.use(cookieParser)
+
+//llamar al router
+app.use('/', require('./routes/router'))
+
+
+app.listen(3000, ()=>{
+    console.log('SERVER UP runnung in http://localhost:3000')
+})
